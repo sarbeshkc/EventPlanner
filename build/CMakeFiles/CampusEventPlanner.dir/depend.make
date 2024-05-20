71eb007a8c74bd079e8ed4dd330ee8d568b367d2
sarbeshkc <weebmaniac2314@gmail.com>
2024-05-20 07:28:45 +0545

    Setting up project

---
+++ build/CMakeFiles/CampusEventPlanner.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CampusEventPlanner.
+# This may be replaced when dependencies are built.
---
+++ build/_deps/sqlite3-subbuild/CMakeFiles/sqlite3-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqlite3-populate"
+  "CMakeFiles/sqlite3-populate-complete"
+  "sqlite3-populate-prefix/src/sqlite3-populate-stamp/sqlite3-populate-build"
+  "sqlite3-populate-prefix/src/sqlite3-populate-stamp/sqlite3-populate-configure"
+  "sqlite3-populate-prefix/src/sqlite3-populate-stamp/sqlite3-populate-download"
+  "sqlite3-populate-prefix/src/sqlite3-populate-stamp/sqlite3-populate-install"
+  "sqlite3-populate-prefix/src/sqlite3-populate-stamp/sqlite3-populate-mkdir"
+  "sqlite3-populate-prefix/src/sqlite3-populate-stamp/sqlite3-populate-patch"
+  "sqlite3-populate-prefix/src/sqlite3-populate-stamp/sqlite3-populate-test"
+  "sqlite3-populate-prefix/src/sqlite3-populate-stamp/sqlite3-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sqlite3-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
